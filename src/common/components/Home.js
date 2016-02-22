0048f5daf684ae53eec69788fa01857843b83c78
zhaojunan <anzi@zja.zja>
2016-02-21 23:41:28 -0500

    完成首页的介绍

--- src/common/components/Home.js
+++ src/common/components/Home.js
@@ -20,66 +20,89 @@ class Home extends Component {
       	<div className="posts">
   
   			<div className="post banner">
-			    <h1 className="post-title">I build <em>scalable</em>, <em>maintainable</em> and <em>secure</em> enterprise web applications.</h1>
-    			<p>for <em>agencies</em>, <em>bluechips</em>, <em>start-ups</em> and sometimes, <em>myself</em>. <a href="#" onClick={this.eventToggleSidebar}> Find out More</a></p>
+			    <h1 className="post-title">本人男，仍单着，喜欢罗宾这样知性的女孩。作为一位长期待在小黑屋的程序员，有志于利用技术改变一些事情。现从事前端开发，关注<em>可复用</em>, <em>可维护</em> <em>高性能</em>  <em>最新的</em> 技术发展。对js近几年发展充满惊讶</h1>
 
 			</div>
   
 		  	<div className="post clearfix">
 
-				<h2>Technologies I build with:</h2>
+				<h2>应用过的技能:</h2>
 
 				<div className="skill-item clearfix">
-					<h4>Client Side JS</h4>
+					<h4>前端开发</h4>
 					<ul className="">
-						<li><em>Backbone</em></li>
+						<li><em><b>*</b><b>*</b>angular</em>
+						</li>
+						<li><em>requirejs</em>
+						</li>
+						<li><em>bootstrap</em>
+						</li>
+						<li><em>sass</em>
+						</li>
 						<li><em><b>*</b><b>*</b>React</em>
 							<ul>
 								<li><em><b>*</b>Redux</em></li>
-								<li><em>Flux</em></li>
 							</ul>
 						</li>
-						<li><em>D3</em></li>
-					</ul>
-				</div>
-
-
-				<div className="skill-item clearfix">
-					<h4>Server Side JS</h4>
-					<ul>
-						<li><em>MongoDB</em></li>
-						<li><em><b>*</b>Node</em>
+						<li><em><b>*</b><b>*</b>Restful api</em>
+						</li>
+						<li><em>node</em>
 							<ul>
 								<li><em><b>*</b>Express</em></li>
 								<li><em>Hapi</em></li>
 							</ul>
 						</li>
+
+						<li><em>backbone</em>
+						</li>
+					</ul>
+				</div>
+
+
+				<div className="skill-item clearfix">
+					<h4>客户端和服务器端开发</h4>
+					<ul>
+						<li><em><b>*</b><b>*</b>C/C++</em></li>
+						<li><em>MFC</em></li>
+						<li><em>gtk glade3</em></li>
+						<li><em>python </em>
+                            <li><em>twisted</em></li>
+                        </li>
 					</ul>
 				</div>
 
 				<div className="skill-item clearfix">
-					<h4>Testing</h4>
+					<h4>单元测试</h4>
 					<ul>
 						<li><em>Jasmine</em></li>
 						<li><em>Karma</em></li>
-						<li><em>Protractor</em></li>
-						<li><em>Jest</em></li>
+						<li><em>weinre</em></li>
 					</ul>
 				</div>
 
 				<div className="skill-item clearfix">
-					<h4>Deployment</h4>
+					<h4>管理工具</h4>
 					<ul className="clearfix">
-						<li><em><b>*</b>Nginx</em></li>
-						<li><em><b>*</b>Webpack</em></li>
-						<li><em>Gulp</em></li>
-						<li><em>Grunt</em></li>
+						<li><em><b>*</b>Grunt</em></li>
+						<li><em>Webpack</em></li>
+						<li><em>npm</em></li>
+						<li><em>bower</em></li>
+					</ul>
+				</div>
+
+				<div className="skill-item clearfix">
+					<h4>系统管理</h4>
+					<ul className="clearfix">
+						<li><em>linux</em></li>
+						<li><em>node</em></li>
+						<li><em><b>*</b>bash</em></li>
+						<li><em><b>*</b>zsh + terminator + vim </em></li>
 					</ul>
 				</div>
 
 				<div className="exclaimation">
-					<em><b>*</b> This site is built with these technologies. View the <a href="https://github.com/caljrimmer/portfolio-redux-app">github repo here</a>.</em><br/>
-					<em><b>**</b> Yep, I know React can be used on the server side too. This site is Isomorphical rendered.</em>
+					<em><b>*</b> github上有一些使用上面相关技术的项目 ,可跳到<a href="https://github.com/anzizhao">anzizhao repo</a>查看</em><br/>
+					<em><b>**</b>这个网站是基于xxxx项目, 使用react和redux技术.</em>
 				</div>
 
   			</div>
@@ -87,16 +110,11 @@ class Home extends Component {
 
   			<div className="post clearfix">
 
-				<h2>Companies I have worked with:</h2>
+				<h2>工作过的公司:</h2>
 
 				<ul className="clients">
-					<li className="goldmans-logo">Goldman Sachs</li>
-					<li className="orange-logo">Orange</li>
-					<li className="tesco-logo">Tesco</li>
-					<li className="timeout-logo">Timeout</li>
-					<li className="wiley-logo">John Wiley and Sons</li>
-					<li className="covestor-logo">Covestor</li>
-					<li className="shipserv-logo">Shipserv</li>
+					<li className="goldmans-logo">杭州海康威视数字技术有公司</li>
+					<li className="orange-logo">考拉先生</li>
 				</ul>
 
 			</div>
